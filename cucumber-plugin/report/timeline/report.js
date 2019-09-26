d882c48e0855e92b802b2d235e34afa62987e823
grasshopper7 <grass.hopper.moc@gmail.com>
2019-09-27 00:12:48 +0530

    Added reports

---
+++ cucumber-plugin/report/timeline/report.js
@@ -0,0 +1,65 @@
+$(document).ready(function() {
+CucumberHTML.timelineItems.pushArray([
+  {
+    "id": "scenario-outlines-feature-file;scenario-outline-row-\u003crow-num\u003e;;3",
+    "feature": "Scenario Outlines feature file",
+    "scenario": "Scenario Outline Row 2",
+    "start": 1569522413182,
+    "end": 1569522413189,
+    "group": 1,
+    "content": "",
+    "className": "passed",
+    "tags": ""
+  },
+  {
+    "id": "scenario-outlines-feature-file;scenario-outline-row-\u003crow-num\u003e;;2",
+    "feature": "Scenario Outlines feature file",
+    "scenario": "Scenario Outline Row 1",
+    "start": 1569522413122,
+    "end": 1569522413181,
+    "group": 1,
+    "content": "",
+    "className": "passed",
+    "tags": ""
+  },
+  {
+    "id": "exception-feature-file;scenario-number-one",
+    "feature": "Exception feature file",
+    "scenario": "Scenario Number One",
+    "start": 1569522413069,
+    "end": 1569522413113,
+    "group": 1,
+    "content": "",
+    "className": "failed",
+    "tags": ""
+  },
+  {
+    "id": "scenarios-feature-file;scenario-number-two",
+    "feature": "Scenarios feature file",
+    "scenario": "Scenario Number Two",
+    "start": 1569522413204,
+    "end": 1569522413209,
+    "group": 1,
+    "content": "",
+    "className": "passed",
+    "tags": ""
+  },
+  {
+    "id": "scenarios-feature-file;scenario-number-one",
+    "feature": "Scenarios feature file",
+    "scenario": "Scenario Number One",
+    "start": 1569522413190,
+    "end": 1569522413202,
+    "group": 1,
+    "content": "",
+    "className": "passed",
+    "tags": ""
+  }
+]);
+CucumberHTML.timelineGroups.pushArray([
+  {
+    "id": 1,
+    "content": "Thread[main,5,main]"
+  }
+]);
+});
